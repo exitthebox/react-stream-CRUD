a0b5b4365e2c8183fe5e8bc209e66b0d83516b04
exitthebox <csurams@hotmail.com>
2022-04-12 17:19:26 -0600

    auth finised

---
+++ src/Actions/index.js
@@ -0,0 +1,14 @@
+import { SIGN_OUT, SIGN_IN } from "./Types"
+
+export const signIn = (userId) => {
+    return {
+        type: SIGN_IN,
+        payload: userId
+    }
+}
+
+export const signOut = () => {
+    return {
+        type: SIGN_OUT
+    }
+}
